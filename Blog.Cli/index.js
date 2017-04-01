c5226df61a8db031d943d4caf2a809ff2e8b4a4e
Michal Franc <lam.michal.franc@gmail.com>
2017-04-01 19:43:07 +0100

    initial implementation of mos the commands

--- Blog.Cli/index.js
+++ Blog.Cli/index.js
@@ -4,8 +4,11 @@ var shell = require('shelljs');
 var { spawn } = require('child_process');
 var co = require('co');
 var opn = require('opn');
+var prependFile = require('prepend-file');
 
 var prompt = require('co-prompt');
+
+var mainBlogFolder = 'C:\\Users\\mfranc\\DropBox\\blog\\michal-franc.github.io\\';
 
 var spawnChrome = (site) => {
   opn(site);
@@ -17,14 +20,23 @@ var openVim = (file) => {
   var child = spawn(editor, [file, '-c Goyo'], {
     stdio: 'inherit' 
   });
+
+  child.on('exit', function (e, code) {
+      console.log("finished");
+  });
 };
 
 program
-.arguments('<command> [arg1]')
-  .action((command, arg1) => {
+.arguments('<command> [filename]')
+  .action((command, filename) => {
     co(function *() {
       if(command === 'site') {
         spawnChrome('http://mfranc.com/');
+      }
+      
+      if(command === 'local') {
+        // assuming here that blog is running on local all the time, need to somehow run ruby jekyll all the time
+        spawnChrome('http://mfranc.com.local/');
       }
 
       if(command === 'social') {
@@ -34,15 +46,65 @@ program
         spawnChrome('https://devspl.slack.com/');
       }
 
-      if(command === 'vim') {
-        openVim('');
+      if(command === 'idea') {
+       openVim(mainBlogFolder + '_posts\\_draft\\ideas\\' + 'new_idea.md');
       }
 
-      // generate new blog post with a readline prompts
-      // go to draft folder with NerdTree opened
-      // go to ideas folder with -=-
-      // publish moves the draft to blog folder for publish
-      // schedule moves the post to schdeuled folder
+      if(command === 'draft') {
+       openVim(mainBlogFolder + '_posts\\_draft\\' + 'new_draft.md');
+      }
+
+      if(command === 'template') {
+        var title = yield prompt('title:');
+        var summary = yield prompt('summary:');
+        var category = yield prompt('category:');
+        var tags = yield prompt('tags (comma):');
+        var image = yield prompt('image.jpg (images folder):');
+
+        var template = 
+            `---
+layout: post
+title: ${title}
+summary: ${summary}
+date: %date%
+author: Michal Franc
+comments: true
+categories: [${category}]
+tags: [${tags}]
+image: ${image}
+---`;
+
+        if(filename){
+          prependFile(filename, template, function(error) {
+            if (error) {
+                console.error(error.message);
+            }
+
+            console.log('file template added');
+            process.exit();
+          });
+        } 
+      }
+
+      if(command === 'wait') {
+        shell.mv(filename, mainBlogFolder + '_posts\\waiting\\');
+      }
+
+      if(command === 'publish') {
+        var category = yield prompt('category:');
+        var date = yield prompt('date (yyyy-mm-dd hh:mm):');
+
+        shell.sed('-i', '%date%', date, file);
+        shell.mv(filename, mainBlogFolder + '_posts\\' + category);
+      }
+
+      if(command === 'drafts') {
+       openVim(mainBlogFolder + '_posts\\_draft\\');
+      }
+
+      if(command === 'ideas') {
+       openVim(mainBlogFolder + '_posts\\_draft\\ideas\\');
+      }
     })
   })
 .parse(process.argv);
