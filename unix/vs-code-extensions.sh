4d2092290c19429664eb0fc8aca4bd0bb97d211f
Michal Franc <lam.michal.franc@gmail.com>
2018-11-29 18:37:53 +0000

    Vs Code plugins update

---
+++ unix/vs-code-extensions.sh
@@ -0,0 +1,5 @@
+code --install-extension dracula-theme.theme-dracula
+code --install-extension ms-python.python
+code --install-extension ms-vscode.csharp
+code --install-extension sleistner.vscode-fileutils
+code --install-extension vscodevim.vim
