5907dd8f99018cffdc3729f8570f9a0da1a1f19b
Michal Franc <lam.michal.franc@gmail.com>
2018-12-14 23:53:14 +0000

    Fixing the crontab script

--- unix/wallpapers/install.sh
+++ unix/wallpapers/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(crontab -l ; echo "5 * * * * feh --randomize --bg-scale ~/dotfiles/unix/wallpapers/*.jpg")| crontab -
+(crontab -l ; echo "*/1 * * * * DISPLAY=:0 feh --randomize --bg-scale ~/dotfiles/unix/wallpapers/*.jpg >> ~/cron.log")| crontab -
