2252a81653f6a5068c927e15fcaa29514d95af3e
Michal Franc <lam.michal.franc@gmail.com>
2018-12-15 14:45:43 +0000

    using bg-fill for feh + autoreload name

---
+++ images/wallpapers/auto-reload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+(crontab -l ; echo "*/10 * * * * DISPLAY=:0 feh --randomize --bg-fill $HOME/dotfiles/unix/wallpapers/*.jpg >> $HOME/cron.log")| crontab -
--- images/wallpapers/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-(crontab -l ; echo "*/10 * * * * DISPLAY=:0 feh --randomize --bg-scale $HOME/dotfiles/unix/wallpapers/*.jpg >> $HOME/cron.log")| crontab -
