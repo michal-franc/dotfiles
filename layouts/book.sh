a359a88a8ff0e2cf6f7c97ba265cb12e86d33a69
Michal Franc <lam.michal.franc@gmail.com>
2018-12-23 14:09:29 +0000

    Experimental layout for reading books

--- layouts/book.sh
+++ layouts/book.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-i3-msg "workspace 4; append_layout /home/mfranc/layouts/book.json"
-termite & termite & termite & zathura
+if [ -z "$1" ]
+then
+  echo "param 1 -> No book path supplied"
+  exit 1
+fi
+
+workspace=$2
+
+if [ -z "$2" ]
+then
+  echo "No workspace supplied - assuming default 10: Book"
+  workspace="10: Book"
+fi
+
+i3-msg "workspace $workspace; append_layout /home/mfranc/layouts/book.json"
+
+termite -d $HOME/notes/books &
+termite &
+termite & 
+zathura $1 &
