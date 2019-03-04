380d540e316b5ba05a714913b04e63180b33cf84
Michal Franc <lam.michal.franc@gmail.com>
2019-03-04 01:15:54 +0000

    small fixes to recurrent sscript

--- recurrent.sh
+++ recurrent.sh
@@ -5,7 +5,9 @@ do
   count=$(task status:pending $line +recurrent count)
   if [[ $count -eq 0 ]]; then
     echo creating missing $line
-    task add $line +recurrent
+    task add $line +recurrent due:week
+  else
+    echo $line in on the list
   fi
 done <~/todo/recurrent.md
 
