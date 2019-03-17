99377e1e2c3fd7293f88f8c69a525466c439cbeb
Michal Franc <lam.michal.franc@gmail.com>
2019-03-17 00:27:37 +0000

    fixing code extension installer, laayout social add hacker news fix to contex

--- .config/Code/reload-vs-code-extensions.sh
+++ .config/Code/reload-vs-code-extensions.sh
@@ -1,1 +1,3 @@
-code --list-extensions | xargs -L 1 echo code --install-extensions >> install-vs-code-extensions.sh
+#!/bin/bash
+
+code --list-extensions | xargs -L 1 echo "code --install-extension" > install-vs-code-extensions.sh
