3eda58d9f3795fdc9c97a1fdfb7bffa1958f8f07
Michal Franc <michal.franc@form3.tech>
2019-03-18 17:45:22 +0000

    fix for accidental capslock change

--- .config/startup.sh
+++ .config/startup.sh
@@ -7,6 +7,8 @@ setxkbmap -option caps:backspace
 setxkbmap -option shift:both_capslock
 
 xmodmap -e "clear Lock"
+# just in case caps lock on ctrl r
+xmodmap -e "keycode 105 = Caps_Lock"
 
 xmodmap -e "keycode 108 = space"
 xmodmap -e "clear mod1"
