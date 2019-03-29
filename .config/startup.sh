7a6b959685bbe16acd0fb1bb649a3d290a1558f8
Michal Franc <michal.franc@form3.tech>
2019-03-29 08:28:47 +0000

    Vs code spell check settings and echo in startup script

--- .config/startup.sh
+++ .config/startup.sh
@@ -3,6 +3,8 @@
 xrandr --output DP-1-1 --right-of eDP-1-1
 xrandr --output DP-1-1 --brightness 0.7
 
+echo monitors set
+
 setxkbmap -option caps:backspace
 setxkbmap -option shift:both_capslock
 
@@ -14,3 +16,5 @@ xmodmap -e "keycode 108 = space"
 xmodmap -e "clear mod1"
 
 xset r rate 200 70
+
+echo keyboard settings set
