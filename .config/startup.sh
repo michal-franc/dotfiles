e35a9442351f1bac97113fd25604d4fd9d01a7e6
Michal Franc <michal.franc@form3.tech>
2019-09-23 16:34:01 +0100

    chnage capslock to home button

--- .config/startup.sh
+++ .config/startup.sh
@@ -11,7 +11,7 @@ setxkbmap -option shift:both_capslock
 
 xmodmap -e "clear Lock"
 # just in case caps lock on ctrl r
-xmodmap -e "keycode 78 = Caps_Lock"
+xmodmap -e "keycode 110 = Caps_Lock"
 
 xmodmap -e "keycode 108 = space"
 xmodmap -e "clear mod1"
