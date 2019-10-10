cc727a8a1ff799491347dd2c0a65fa5b87063306
Michal Franc <michal.franc@form3.tech>
2019-10-10 16:31:11 +0100

    lower the threshold to fire battery alarm

--- scripts/cron/battery_check.sh
+++ scripts/cron/battery_check.sh
@@ -6,6 +6,6 @@ if [ -r "$HOME/.dbus/Xdbus" ]; then
 fi
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 ]] ; then
     DISPLAY=:0.0 /usr/bin/notify-send -u critical "low battery" "$BATTINFO"
 fi
