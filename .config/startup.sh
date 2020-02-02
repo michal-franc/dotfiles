abd4b11ffe4df565b392ca1a42b92e56fefcf021
Michal Franc <michal.franc@form3.tech>
2020-02-02 18:44:52 +0100

    New monitor setup

--- .config/startup.sh
+++ .config/startup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-xrandr --output HDMI-0 --auto
-xrandr --output HDMI-0 --above eDP-1-1
-xrandr --output DP-1-1 --auto
-xrandr --output DP-1-1 --left-of HDMI-0
-xrandr --output DP-1-1 --brightness 0.9
-xrandr --output HDMI-0 --brightness 0.9
-xrandr --output eDP-1-1 --brightness 0.9
+xrandr --output DP-1-1 --primary --auto --brightness 0.9 --rotate normal
+xrandr --output eDP-1-1 --auto --below DP-1-1 --brightness 0.9 --rotate normal 
+xrandr --output HDMI-0 --auto --right-of DP-1-1 --rotate normal --brightness 0.9
 
 echo monitors set
 
