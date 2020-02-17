3883099cb15dd58445361099a236472843617f13
Michal Franc <michal.franc@form3.tech>
2020-02-17 11:03:58 +0100

    adapting autoreaload to new wall_reload script

--- images/wallpapers/auto-reload.sh
+++ images/wallpapers/auto-reload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(crontab -l ; echo "*/10 * * * * DISPLAY=:0 feh --randomize --bg-fill $HOME/dotfiles/unix/wallpapers/*.jpg >> $HOME/cron.log")| crontab -
+(crontab -l ; echo "*/10 * * * * DISPLAY=:0 feh --randomize --bg-fill $HOME/dotfiles/unix/wallpapers/* >> $HOME/cron.log")| crontab -
