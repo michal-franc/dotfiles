d31076b39480f01afacf59362883510b373ceca3
Michal Franc <lam.michal.franc@gmail.com>
2022-07-04 11:35:02 +0200

    comment space override

--- .config/keyboard.sh
+++ .config/keyboard.sh
@@ -8,8 +8,8 @@ xmodmap -e "clear Lock"
 # just in case caps lock on ctrl r
 xmodmap -e "keycode 110 = Caps_Lock"
 
-xmodmap -e "keycode 108 = space"
-xmodmap -e "clear mod1"
+# xmodmap -e "keycode 108 = space"
+# xmodmap -e "clear mod1"
 
 xset r rate 200 70
 notify-send -t 1000 "keyboard settings set"
