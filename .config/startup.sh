fcd55142d6d7bebd514857130c934c6133aa8fe7
Michal Franc <lam.michal.franc@gmail.com>
2022-08-21 13:17:46 +0200

    adding trackball settings

--- .config/startup.sh
+++ .config/startup.sh
@@ -15,6 +15,8 @@ while inotifywait /tmp/keyboard.lock; do { bash $HOME/.config/keyboard.sh; }; do
 xinput --set-prop "Logitech Wireless Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
 xinput --set-prop "Logitech M215" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
 
+xinput -set-button-map "ExpertBT5.0 Mouse" 1 1 1 4 5 6 7 3 9
+xinput --set-prop "ExpertBT5.0 Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
 
 touch $HOME/.dbus/Xdbus
 chmod 666 $HOME/.dbus/Xdbus
