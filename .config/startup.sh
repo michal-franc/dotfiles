20c351b4d28b6ab5595bc32da5fc6a6545df00ba
Michal Franc <lam.michal.franc@gmail.com>
2024-07-21 11:00:16 +0200

    reconfiguring monitor setup

--- .config/startup.sh
+++ .config/startup.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-xrandr --output DP-1 --primary --auto --brightness 1 --rotate normal
-xrandr --output eDP-1 --auto --below DP-1 --rotate normal --brightness 1
-xrandr --output DP-2 --auto --left-of DP-1 --brightness 1 --rotate normal 
+xrandr --output Virtual1 --primary --auto --brightness 1 --rotate normal
+xrandr --output Virtual2 --auto --left-of Virtual1 --brightness 1 --rotate normal 
 
 echo monitors set
 
-xset s off -dpms
+# xset s off -dpms
 
-bash $HOME/.config/keyboard.sh
-while inotifywait /tmp/keyboard.lock; do { bash $HOME/.config/keyboard.sh; }; done&
+#bash $HOME/.config/keyboard.sh
+#while inotifywait /tmp/keyboard.lock; do { bash $HOME/.config/keyboard.sh; }; done&
 
 # lowering mouse acceleration
-xinput --set-prop "Logitech Wireless Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
-xinput --set-prop "Logitech M215" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
+#xinput --set-prop "Logitech Wireless Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
+#xinput --set-prop "Logitech M215" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
 
-xinput -set-button-map "ExpertBT5.0 Mouse" 1 1 1 4 5 6 7 3 9
-xinput --set-prop "ExpertBT5.0 Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
+#xinput -set-button-map "ExpertBT5.0 Mouse" 1 1 1 4 5 6 7 3 9
+#xinput --set-prop "ExpertBT5.0 Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
 
-touch $HOME/.dbus/Xdbus
-chmod 666 $HOME/.dbus/Xdbus
-env | grep DBUS_SESSION_BUS_ADDRESS > $HOME/.dbus/Xdbus
-echo 'export DBUS_SESSION_BUS_ADDRESS' >> $HOME/.dbus/Xdbus
+#touch $HOME/.dbus/Xdbus
+#chmod 666 $HOME/.dbus/Xdbus
+#env | grep DBUS_SESSION_BUS_ADDRESS > $HOME/.dbus/Xdbus
+#echo 'export DBUS_SESSION_BUS_ADDRESS' >> $HOME/.dbus/Xdbus
 
 exit 0
