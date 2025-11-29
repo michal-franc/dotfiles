e617281eee4294f3b09e899ce42496eb5fbb701a
Michal Franc <lam.michal.franc@gmail.com>
2025-11-29 15:53:26 +0100

    new setup for new machine monitor setup

--- .config/startup.sh
+++ .config/startup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-xrandr --output Virtual2 --primary --auto --brightness 1 --rotate normal 
-# xrandr --output Virtual1 --auto --right-of Virtual2 --brightness 1 --rotate normal
+xrandr --output DP-1 --primary --auto --brightness 1 --rotate normal 
+xrandr --output DP-1-1 --auto --left-of DP-1 --brightness 1 --rotate left
 
-echo monitors set
+#echo monitors set
 
 # xset s off -dpms
 
-#bash $HOME/.config/keyboard.sh
+bash $HOME/.config/keyboard.sh
 #while inotifywait /tmp/keyboard.lock; do { bash $HOME/.config/keyboard.sh; }; done&
 
-# lowering mouse acceleration
-#xinput --set-prop "Logitech Wireless Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
-#xinput --set-prop "Logitech M215" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
+xinput --set-prop "Logitech Gaming Mouse G502" "Coordinate Transformation Matrix" 1.5 0 0 0 1.5 0 0 0 1
 
 #xinput -set-button-map "ExpertBT5.0 Mouse" 1 1 1 4 5 6 7 3 9
 #xinput --set-prop "ExpertBT5.0 Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
